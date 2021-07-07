4c2d647b09f1cef374834bf1013f662dbdec12b4
Macmaxwell <82811326+Macmaxwell@users.noreply.github.com>
2021-07-07 16:05:41 +0200

    week 1 deberes

---
+++ week-2/InClass/exercise-D.js
@@ -0,0 +1,12 @@
+function moodChecker(great) {
+    if (great === "Happy") {
+      return `${great} Good job,you are doing great!`;
+    } else if (great === "sad") {
+      return `${great} Every cloud has a silver lining`;
+    } else if (great === number) {
+      return `${num} is less than 20`;
+    } else {
+      return `${num} isn't even a number :(`;
+    }
+  }
+  console.log(moodChecker(great));
